b397b64a38371b9c1d6fad700cf666f6c5c42b39
mohamedslima <hammaslima@gmail.com>
2022-05-03 10:39:55 +0200

    completed exercise

---
+++ js/script.js
@@ -0,0 +1,144 @@
+let $ = document.querySelector.bind(document);
+
+const icons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+
+generateBox(icons);
+
+let typeOfIcon = $('#type-of-icon');
+
+typeOfIcon.addEventListener("change", () => {
+
+	// ripulisco il container
+    $('.container').innerHTML = "";
+    const typeSelected = typeOfIcon.value;
+	let filteredArray = icons.filter((element) => typeSelected === 'all' || typeSelected === element.type);
+	console.log(typeSelected);
+	console.log(filteredArray);
+	generateBox(filteredArray);
+});
+
+function generateBox(array) {
+	array.forEach((element)=> {
+		let icon = `          
+		  <div class="box">
+			<i class="${element.family} ${element.prefix}${element.name}" style="color: ${element.color}"></i>
+			<h5>${element.name}</h5>
+		  </div>
+		`;
+		// stampo in html 
+		$('.container').innerHTML += icon;
+	})	
+}
